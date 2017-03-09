cfc8138b93d5a87750374c703827afde3e41bdc9
[L]earned Media <learnedmedia@Learneds-Mac-mini.local>
2017-03-09 15:38:52 -0500

    Set up livereload

--- wp-content/themes/lundy-law/gulpfile.js
+++ wp-content/themes/lundy-law/gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('default', function() {
-  
+
 });
 
 gulp.task('sass', function () {
 	return gulp.src('./development/styles/style.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./'));
-});  
+		.pipe(gulp.dest('./'))
+		.pipe(livereload());
+});
+
+//livereload any php edits
+gulp.task('php', function() {
+	gulp.src('./*.php')
+		.pipe(livereload());
+});
 
 gulp.task('watch', function () {
+	livereload.listen();
 	gulp.watch('./development/styles/*.scss', ['sass'] );
+	gulp.watch('./*.php', ['php']);
 });
