436a5068c7d49426fad93e1328b09a87b32815ba
[L]earned Media <learnedmedia@Learneds-Mac-mini.local>
2017-03-09 12:06:04 -0500

    updated plugin(s) - yeast seo

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit0b36b471ac303c879fed046d17914a59::getLoader();
+return ComposerAutoloaderInita28c17fcbbdfaa3b59475992c1d59b05::getLoader();
